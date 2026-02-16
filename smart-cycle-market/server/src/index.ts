f6c8ef8dd6aaf0b20d89c2c35e9d93fee3053da5
asnganje <abd.nganje@gmail.com>
2026-02-16 20:06:57 +0300

    restructure folders and run home route test

--- smart-cycle-market/server/src/index.ts
+++ smart-cycle-market/server/src/index.ts
@@ -1,4 +1,13 @@
-const a = 5;
-const b =4;
+import express from "express"
+import { log } from "node:console"
 
-console.log(a+b);
+const app = express()
+app.get("/", (req, res)=> {
+  res.send("<h1>Hello from server</h1>")
+})
+const port = 3000
+app.listen(port, ()=> {
+  console.log(`Server is listening on port ${port}`);
+  
+})
+
