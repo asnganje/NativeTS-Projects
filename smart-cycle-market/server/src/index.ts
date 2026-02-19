f8b235fae24f652779f7dbbb4d995eeaf26b0333
asnganje <abd.nganje@gmail.com>
2026-02-19 15:32:29 +0300

    call dotenv in app entry index file

--- smart-cycle-market/server/src/index.ts
+++ smart-cycle-market/server/src/index.ts
@@ -1,16 +1,33 @@
 import express from "express";
 import authRouter from "./routes/auth";
-import { sign_up } from "./controllers/auth";
+import { connectDB } from "./db";
+import dotenv from "dotenv"
+
+dotenv.config()
+
 // import bodyParser from "body-parser"
 
 const app = express();
 app.use(express.json());
+
 // reading content from a form
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authRouter);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+const uri = process.env.URI
+
+const start = async() => {
+  try {
+    await connectDB(uri)
+    app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
+  } catch (error) {
+    console.log("Error starting up the app");
+    
+  }
+}
+
+start()
