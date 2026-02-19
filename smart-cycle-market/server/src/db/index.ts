c8649b8e748e18ec385b3b8941ae6e71505856f1
asnganje <abd.nganje@gmail.com>
2026-02-19 15:41:00 +0300

    fix await issue

--- smart-cycle-market/server/src/db/index.ts
+++ smart-cycle-market/server/src/db/index.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 
-export const connectDB = (uri: string) => {
-  mongoose.connect(uri)
+export const connectDB = async (uri: string): Promise<void> => {
+  await mongoose.connect(uri)
 } 
